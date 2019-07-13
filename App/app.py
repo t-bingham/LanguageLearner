a1c0d7f6e99897cdd6b2966da1472bd70b00693e
t-bingham <binghamtd@gmail.com>
2019-07-14 01:22:09 +1200

    App layout

---
+++ App/app.py
@@ -0,0 +1,23 @@
+import random
+import os
+import argparse
+import pygame
+
+def display():
+    continue
+
+def test():
+    continue
+
+def extract():
+    continue
+
+def parser():
+    continue
+
+def main():
+    continue
+
+
+if __name__ == "__main__":
+    main()
